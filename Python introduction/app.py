cbc27c637304672c6224d59ae879f895e77154bb
OlegColeg <ooale47@gmail.com>
2025-02-17 10:23:58 +0200

    connect git

---
+++ Python introduction/app.py
@@ -0,0 +1,12 @@
+number=[]
+while True:
+    try:
+        n = int(input("Enter a number: "))
+        if n == 0:
+            print("Nr 0 a fost introdus")
+            break
+        number.append(n)
+    except ValueError:
+        print("Introduceti un nr intreg")
+        break
+    print(number)
