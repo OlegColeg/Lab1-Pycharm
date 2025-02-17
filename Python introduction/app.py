29a96b93df80c26f7625da04b462379ecab84b59
OlegColeg <ooale47@gmail.com>
2025-02-17 10:35:09 +0200

    connect git si primul cod

--- Python introduction/app.py
+++ Python introduction/app.py
@@ -11,3 +11,16 @@ while True:
         break
     print(number)
 //primul code
+    if not number:
+        print("Lista este goala")
+    else:
+        number.sort()
+        print("Lista ordonata crescator: ", number)
+        print("Elementul minim: ", number[0])
+        print("Elementul minim: ", min(number))
+        print("Elementul minim: ", number[-1])
+        print("Elementul minim: ", max(number))
+        print("Elementul minim: ", sum(number))
+        print("Media Aritmetica: ",
+              sum(number) / len(number)
+        print("Total elemente: ", len(number))
